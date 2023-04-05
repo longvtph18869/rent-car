0238900dd1de7c56ee496c92c2a5284ad8b0d4bd
longvtph18869 <longvutien15092000@gmail.com>
2023-04-05 11:18:52 +0700

    first

---
+++ rent-car-server/src/main/java/com/example/rent/RentCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarApplication.class, args);
+	}
+
+}
