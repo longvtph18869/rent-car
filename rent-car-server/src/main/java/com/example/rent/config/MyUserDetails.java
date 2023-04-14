ae3b510108950609cd1ca4b09c96e6418f149bd0
Trung-Luong-1607 <60788442+Trung-Luong-1607@users.noreply.github.com>
2023-04-14 10:15:25 +0700

    api-login

---
+++ rent-car-server/src/main/java/com/example/rent/config/MyUserDetails.java
@@ -0,0 +1,63 @@
+package com.example.rent.config;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.rent.entities.User;
+
+public class MyUserDetails implements UserDetails {
+	User user;
+
+	public MyUserDetails(User user) {
+		super();
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
